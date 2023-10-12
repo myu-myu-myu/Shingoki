2e9ce8912dd2e16716c5831ae2df3676657d22cf
myu-myu-myu <128329553+myu-myu-myu@users.noreply.github.com>
2023-10-13 04:49:55 +0900

    Update app.js
    app.jsファイルを更新！

--- precourse.bacefook-dig/app.js
+++ precourse.bacefook-dig/app.js
@@ -9,91 +9,85 @@ window.addEventListener("load", () => {
     localStorage.setItem("password", password);
   }
   reloadPost();
-  const user_display = document.getElementById('login_name');
-  user_display.innerText = username;
+  const user_icon = document.getElementById('login_icon');
+  const user_name = document.getElementById('login_name');
+  user_icon.src = `images/icon0.png`;
+  user_name.innerText = `Hi, ${username}!`;
 });
 
-// DOM作成後はイベント発火を待つ
-document.addEventListener('DOMContentLoaded', function () {
-  const articleForm = document.getElementById('articleForm');
 
-  // Postボタンを押すと
-  articleForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const text = document.getElementById('articleContent').value;
-    const feeling = document.getElementById('articleFeeling').value;
-    console.log("get : ", document.getElementById('articlePicture'));
-    const pic = "pic0.png";// document.getElementById('articlePicture').value;
-    document.getElementById('articleContent').value = '';
-    document.getElementById('articleFeeling').value = '';
-    // document.getElementById('articlePicture').value = '';
-    const timestamp = new Date();
-    const article = {
-      friend: localStorage.getItem("username"),
-      text: text,
-      feeling: feeling,
-      image: pic,
-      timestamp: timestamp
-    };
-    pushPost(article);
-  });
-
-  // Cancelボタンを押すと
-  articleForm.addEventListener('reset', function (e) {
-    e.preventDefault();
-    document.getElementById('articleContent').value = '';
-    document.getElementById('articleFeeling').value = '';
-    document.getElementById('articlePicture').value = '';
+// モーダル画面
+const m_btn = document.getElementById('btn_m_open');//Postボタン
+const m_ = document.querySelector('.m_overlay');//モーダル全体
+const closebtn_ = document.getElementById('m_close');//Casncelボタン
+const body_ = document.getElementsByTagName('body')[0];
+// Postボタン1を押すとモーダル画面表示
+m_btn.addEventListener('click', function () {
+  body_.classList.add('no_scroll');//スクロールさせない
+  m_.classList.add('active');
+  // ❌ボタンを押すとモーダル画面終了
+  closebtn_.addEventListener('click', function () {
+    m_.classList.remove('active');
+    body_.classList.remove('no_scroll');
+  }, false);
+}, false);
 
 
-    console.log("filename : ", filename);
-  });
-
-});
-
-// 画像のドラッグアンドドロップ 未完。。。
-const target = document.getElementById('articlePicture');
-
-target.addEventListener('dragover', function (e) {
-  e.preventDefault();
-  e.stopPropagation();
+// 画像のドラッグアンドドロップ
+const articlePic = document.getElementById('articlePicture');
+let result = "";
+articlePic.addEventListener('dragover', function (e) {
+  e.preventDefault();//意図的にブラウザで既定されている動作をキャンセル
+  e.stopPropagation();//他のListenerへのイベントの伝播をキャンセル
   e.dataTransfer.dropEffect = 'copy';
 });
-
-const filename = target.addEventListener('drop', function (e) {
+articlePic.addEventListener('drop', function (e) {
   e.stopPropagation();
   e.preventDefault();
   const reader = new FileReader();
-  reader.onload = function (e) {
+  reader.onload = function (e) {//loadできたら以下関数を実行
     document.getElementById('preview').src = e.target.result;
+    result = e.target.result;
   }
   reader.readAsDataURL(e.dataTransfer.files[0]);
-
-
-  console.log("check : ", e.dataTransfer.files[0].name);
-  return e.dataTransfer.files[0].name;
 });
-console.log("filename : ", filename);
 
-// モーダル
-const modal_btn = document.getElementById('btn-modal-open');//Let's Postボタン
-const modal_01 = document.querySelector('.modal-overlay');//モーダル全体
-const closeBtn_01 = document.getElementById('modal-close');//cancelボタン
-const body_01 = document.getElementsByTagName('body')[0];
-console.log(body_01);
-modal_btn.addEventListener('click', function() {
-  body_01.classList.add('no_scroll');//スクロールさせない
-  modal_01.classList.add('active');
-   closeBtn_01.addEventListener('click', function() {
-      modal_01.classList.remove('active');
-      body_01.classList.remove('no_scroll');
-   }, false);
-   //Escapeを押した時に閉じるようにする
-   document.addEventListener('keydown', keydown_ivent);
-   function keydown_ivent(e) {
-      if(e.key == 'Escape'){
-         modal.classList.remove('active');
-        body_01.classList.remove('no_scroll');
-      }
-   }
-}, false);
+
+// DOM作成後、イベント発火に備えて待機
+document.addEventListener('DOMContentLoaded', function () {
+  const articleForm = document.getElementById('articleForm');
+  // Postボタン2の処理
+  articleForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const text = document.getElementById('articleContent').value;
+    const tag = document.getElementById('articleTag').value;
+    const feeling = document.getElementById('articleFeeling').value;
+    const pic = result;
+    document.getElementById('articleContent').value = '';
+    document.getElementById('articleTag').value = '';
+    document.getElementById('articleFeeling').value = '';
+    document.getElementById('articlePicture').value = 'Drag & Drop Here!';
+    document.getElementById('preview').src = '';
+    const timestamp = new Date();
+    const article = {
+      id: idCount(),
+      friend: localStorage.getItem("username"),
+      friend_icon: "icon0.png",
+      text: text,
+      tag: tag,
+      feeling: feeling,
+      image: pic,//"pic0.png",
+      timestamp: timestamp
+    };
+    pushPost(article, pic);
+    result = "";
+  });
+  // ❌ボタンの処理
+  articleForm.addEventListener('reset', function (e) {
+    e.preventDefault();
+    document.getElementById('articleContent').value = '';
+    document.getElementById('articleTag').value = '';
+    document.getElementById('articleFeeling').value = '';
+    document.getElementById('articlePicture').value = '';
+  });
+});

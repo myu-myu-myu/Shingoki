9d43dbd97bc35fd31e4dfafcc91904e00f35582f
toranosuke kakizono <136287926+toranosuke-kakizono@users.noreply.github.com>
2023-10-13 11:29:27 +0900

    Update dataGenerator.js

--- precourse.bacefook-dig/dataGenerator.js
+++ precourse.bacefook-dig/dataGenerator.js
@@ -1,37 +1,23 @@
-let count = -1;
-const idCount = () => {
-  count++;
-  return count;
-};
-
-function newPost(post, containerEl, val_add, val_pic) {
-  const postEl = document.createElement("article");
+function newPost(post, containerEl, val) {
+  const postEl = document.createElement("div");
   postEl.className = "one_post";
-  // friend情報をランダム生成
-  const post_El = document.createElement("div");
-  post_El.className = "friend_info";
-  const iconEl = document.createElement("img");
-  iconEl.className = "friend_icon";
-  iconEl.src = `images/${post.friend_icon}`;
-  iconEl.alt = post.friend_icon;
-  post_El.append(iconEl);
+
   const friendEl = document.createElement("div");
   friendEl.className = "friend_name";
   friendEl.innerText = post.friend;
-  post_El.append(friendEl);
-  postEl.append(post_El);
+  postEl.append(friendEl);
 
   const timeEl = document.createElement("div");
-  timeEl.className = `post_time post_time_${post.id}`;
+  timeEl.className = "post_time";
   const time_diff = moment().diff(post.timestamp);
-  const time = Math.floor(time_diff / 1000);
-  if (time < 60) {
-    // timeEl.innerText = `posted a while ago`;
-    timeEl.innerText = `posted ${Math.floor(time)} seconds ago`;
-  } else if (time < 60 * 60) {
-    timeEl.innerText = `posted ${Math.floor(time / 60)} minites ago`;
+  const time_sec = Math.floor(time_diff / 1000);
+  if (time_sec < 60) {
+    // timeEl.innerText = `posted ${time_sec} seconds ago`;
+    timeEl.innerText = `posted a while ago`;
+  } else if (time_sec < 60 * 60) {
+    timeEl.innerText = `posted ${Math.floor(time_sec / 60)} minites ago`;
   } else {
-    timeEl.innerText = `posted ${Math.floor(time / 60 / 60)} hours ago`;
+    timeEl.innerText = `posted ${Math.floor(time_sec / 60 / 60)} hours ago`;
   }
   postEl.append(timeEl);
 
@@ -40,11 +26,6 @@ function newPost(post, containerEl, val_add, val_pic) {
   textEl.innerText = post.text;
   postEl.append(textEl);
 
-  const tagEl = document.createElement("div");
-  tagEl.className = "tag";
-  tagEl.innerText = post.tag;
-  postEl.append(tagEl);
-
   const feelingEl = document.createElement("div");
   feelingEl.className = "feeling";
   feelingEl.innerText = post.feeling;
@@ -52,39 +33,42 @@ function newPost(post, containerEl, val_add, val_pic) {
 
   const imageEl = document.createElement("img");
   imageEl.className = "picture";
-  console.log("post.image.length :", post.image.length);
-  if (val_pic || post.image.length > 100) {
-    imageEl.src = post.image;
-  } else {
-    imageEl.src = `images/${post.image}`;
-  }
-  console.log("imageEl.src:", imageEl.src)
-  imageEl.alt = "good picture!";
+  imageEl.src = `images/${post.image}`;
+  imageEl.alt = post.image;
   postEl.append(imageEl);
 
-  val_add
+  val
     ? containerEl.prepend(postEl)
     : containerEl.append(postEl);
 };
 
-let height_before;
-function reloadPost() {// 新規投稿の時に投稿objをすべて更新
+function reloadPost() {
   const containerEl = document.querySelector("#newsfeed");
   containerEl.innerText = "";
   for (let i = bacefook.newsfeed.length - 1; i >= 0; i--) {
     const post = bacefook.newsfeed[i];
     newPost(post, containerEl);
   }
-  // 自動でスクロール移動しないように投稿の高さを計算
+  //前回のHeightと今のHeightの差分を出して、その分だけ移動させる
   const height_after = document.getElementById('newsfeed').scrollHeight;
-  const height_diff = height_after - height_before;
-  window.scrollBy(0, height_diff);
+  const height_difference = height_after - height_before
+  height_before ? window.scrollBy(0, height_difference) : null;
   height_before = height_after;
+  //new postを表示させる
+  if(newsfeed_counter + 2 <= bacefook.newsfeed.length) {
+    newsfeed_counter = bacefook.newsfeed.length;
+    new_post.classList.add('active');
+  }
 };
 
-function pushPost(post, pic) {// 自分の投稿を投稿objに追加
+//newsfeedの前回Heightを保管する変数
+let height_before;
+//newsfeedをカウントするための変数(初期値６)
+let newsfeed_counter = 6;
+
+function pushPost(post) {
   const containerEl = document.querySelector("#newsfeed");
-  newPost(post, containerEl, 1, pic);
+  newPost(post, containerEl, 1);
   bacefook.newsfeed.push(post);
 };
 
@@ -93,25 +77,22 @@ function pushPost(post, pic) {// 自分の投稿を投稿objに追加
   window.bacefook = {};
   bacefook.newsfeed = [];
   bacefook.friends = {};
-  bacefook.friendNames = [
-    "くの", "さとう", "うだつ", "いむ", "はしだて", "じん", "ふくおか", "たかはし", "うえはら", "ばば",
-    "よこた", "ひらいし", "おがた", "おの", "うえだ", "まつい", "たなか", "こんどう", "あだち", "おおぶち",
-    "かきぞの", "やまだ", "いのうえ", "わきた", "いながき", "かたぎり", "おの", "いはら", "なかくぼ"];
+  bacefook.friendNames = ["ディグダ", "ヒトカゲ", "ミュウ", "ピカチュウ", "ゼニガメ"];
   bacefook.friendNames.forEach(name => {
     bacefook.friends[name] = [];
   });
 
   const starters = [
-    "Totally just",
-    "Just",
-    "Completely",
-    "Waaaaah! i",
-    "I just",
-    "A salaryman",
-    "A salaryman",
-    "Yesterday I",
-    "A ninja",
-    "My boss"
+    "totally just",
+    "just",
+    "completely",
+    "waaaaah! i",
+    "i just",
+    "a salaryman",
+    "a salaryman",
+    "yesterday I",
+    "a ninja",
+    "my boss"
   ];
   const verbs = [
     "ate",
@@ -186,6 +167,7 @@ function pushPost(post, pic) {// 自分の投稿を投稿objに追加
     "#hashtags",
     "#japanlife",
     "#oops",
+    ""
   ];
   const feelings = [
     "happy 😀",
@@ -199,29 +181,21 @@ function pushPost(post, pic) {// 自分の投稿を投稿objに追加
     "excited 🤩",
   ];
   const images = [
-    "pic1.png", "pic2.png", "pic3.png", "pic4.png", "pic5.png",
-    "pic6.png", "pic7.png", "pic8.png", "pic9.png", "pic10.png",
-    "pic11.png", "pic12.png", "pic13.png", "pic14.png", "pic15.png",
-    "pic16.png", "pic17.png", "pic18.png", "pic19.png", "pic20.png",
-  ];
-  const friends_icon = [
-    "icon1.png", "icon2.png", "icon3.png", "icon4.png", "icon5.png",
+    "pic1.png",
+    "pic2.png",
+    "pic3.png",
+    "pic4.png",
+    "pic5.png",
   ];
 
-  // select要素にoption要素を追加 feeling版
+  // select要素にoption要素を追加
   const select_tag = document.getElementById('articleFeeling');
   for (const i of feelings) {
     const optionEl = document.createElement('option');
     optionEl.innerText = i;
+    // optionEl.setAttribute('label', i);
+    // optionEl.setAttribute('value', i);
     select_tag.appendChild(optionEl);
-  };
-
-  // select要素にoption要素を追加 hashtag版
-  const select = document.getElementById('articleTag');
-  for (const i of hashtags) {
-    const optionEl = document.createElement('option');
-    optionEl.innerText = i;
-    select.appendChild(optionEl);
   };
 
   const getRandomElement = array => {
@@ -234,25 +208,23 @@ function pushPost(post, pic) {// 自分の投稿を投稿objに追加
       getRandomElement(starters),
       getRandomElement(verbs),
       getRandomElement(fillers),
-      getRandomElement(nouns)
+      getRandomElement(nouns),
+      getRandomElement(hashtags)
     ].join(" ");
   };
 
   const generatePostObj = timeOffset => {
-    // timeOffset: 投稿を自動生成する時間間隔
+    // timeOffset: 投稿を自動生成する間隔（乱数で生成）
     const timestamp =
       timeOffset
         ? new Date(new Date().getTime() - timeOffset)
         : new Date();
-    // new Date():現在の日時を表す日付objを生成
-    // obj.getTime():1970-01-01 00:00:00(UTC)からのミリ秒で取得
+    // new Date():日時を省略した場合は現在の日時を表す日付objを生成
+    // obj.getTime():objの日時を、1970-01-01 00:00:00(UTC)からのミリ秒で取得
     console.log("投稿文生成：", timestamp);
     return {
-      id: idCount(),
       friend: getRandomElement(bacefook.friendNames),
-      friend_icon: getRandomElement(friends_icon),
       text: generateRandomText(),
-      tag: getRandomElement(hashtags),
       feeling: getRandomElement(feelings),
       image: getRandomElement(images),
       timestamp: timestamp
@@ -271,8 +243,8 @@ function pushPost(post, pic) {// 自分の投稿を投稿objに追加
     addPost(newPost);
   };
 
-  const startCommentNum = 1;
-  for (let i = 0; i < startCommentNum; i++) {// 自動投稿する間隔
+  for (let i = 0; i < 5; i++) {
+    // 自動投稿する間隔、乱数で生成
     const timeOffset = (2 * (10 - i) + Math.random()) * 60 * 60 * 1000;
     createPost(timeOffset);
   }
@@ -280,24 +252,55 @@ function pushPost(post, pic) {// 自分の投稿を投稿objに追加
   const scheduler = () => {
     createPost(null);
     setTimeout(scheduler, (3 + Math.random() * 5) * 1000);
-    setInterval(() => timeReload(bacefook.newsfeed), 1000);
+    // generate a new post every 3 to 8 seconds
   };
 
   scheduler();
+  // function newPostAuto(obj) {
+  //   console.log("post auto! : ", obj)
+  //   const containerEl = document.querySelector("#newsfeed");
+  //   const postEl = document.createElement("div");
+  //   postEl.className = "one_post";
 
-  function timeReload(obj) {
-    for (let i = 0; i < obj.length; i++) {
-      const timeEl = document.querySelector(`.post_time_${i}`);
-      const time = Math.floor(moment(obj[i].timestamp).diff(new Date()) / 1000 * -1);
-      if (time < 60) {
-        // timeEl.innerText = `posted a while ago`;
-        timeEl.innerText = `posted ${Math.floor(time)} seconds ago`;
-      } else if (time < 60 * 60) {
-        timeEl.innerText = `posted ${Math.floor(time / 60)} minites ago`;
-      } else {
-        timeEl.innerText = `posted ${Math.floor(time / 60 / 60)} hours ago`;
-      }
-    }
-  }
+  //   // friend
+  //   const friendEl = document.createElement("div");
+  //   friendEl.className = "friend_name";
+  //   friendEl.innerText = obj.friend;
+  //   postEl.append(friendEl);//子要素として持つ
 
+  //   // time
+  //   const timeEl = document.createElement("div");
+  //   timeEl.className = "post_time";
+  //   const time_diff = moment().diff(obj.timestamp);
+  //   const time_sec = Math.floor(time_diff / 1000);
+  //   if (time_sec < 60) {
+  //     timeEl.innerText = ` ${time_sec} 秒前`;
+  //   } else if (time_sec < 60 * 60) {
+  //     timeEl.innerText = ` ${Math.floor(time_sec / 60)} 分前`;
+  //   } else {
+  //     timeEl.innerText = ` ${Math.floor(time_sec / 60 / 60)} 時間前`;
+  //   }
+  //   postEl.append(timeEl);
+
+  //   // text
+  //   const textEl = document.createElement("div");
+  //   textEl.className = "text";
+  //   textEl.innerText = obj.text;
+  //   postEl.append(textEl);
+
+  //   // feeling
+  //   const feelingEl = document.createElement("div");
+  //   feelingEl.className = "feeling";
+  //   feelingEl.innerText = obj.feeling;
+  //   postEl.append(feelingEl);
+
+  //   // img
+  //   const imageEl = document.createElement("img");
+  //   imageEl.className = "picture";
+  //   imageEl.src = `images/${obj.image}`;
+  //   imageEl.alt = obj.image;
+  //   postEl.append(imageEl);
+
+  //   containerEl.prepend(postEl);
+  // }
 })();
